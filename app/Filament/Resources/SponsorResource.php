efeff91528dda29d800aa556dd1fe2a35768f74d
Jürgen Fitzinger <juergen@bitperfect.at>
2024-03-25 11:46:25 +0100

    Adds sponsors

---
+++ app/Filament/Resources/SponsorResource.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SponsorResource\Pages;
+use App\Models\Sponsor;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Support\Str;
+
+class SponsorResource extends Resource
+{
+    protected static ?string $model = Sponsor::class;
+
+    protected static ?int $navigationSort = 70;
+
+    protected static ?string $navigationIcon = 'heroicon-o-trophy';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('link')
+                    ->url()
+                    ->required()
+                    ->maxLength(512),
+                Forms\Components\FileUpload::make('logo')
+                    ->image()
+                    ->directory('sponsors/logos')
+                    ->required()
+                    ->columnSpanFull(),
+
+                Forms\Components\FileUpload::make('banner_image')
+                    ->image()
+                    ->directory('sponsors/banners')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\TextInput::make('banner_link')
+                    ->url()
+                    ->required()
+                    ->maxLength(512),
+                Forms\Components\Select::make('category')
+                    ->required()
+                    ->options([
+                        'main sponsor' => 'Main sponsor',
+                        'european partner' => 'European partner',
+                        'national partner' => 'National partner',
+                        'media' => 'Media',
+                        'other' => 'Other',
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\ImageColumn::make('logo')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('link')
+                    ->copyable()
+                    ->searchable(),
+                Tables\Columns\ImageColumn::make('banner_image')
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('banner_link')
+                    ->toggleable(isToggledHiddenByDefault: true)
+                    ->copyable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('category')
+                    ->searchable()
+                    ->formatStateUsing(fn ($state) => Str::ucfirst($state)),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSponsors::route('/'),
+            'create' => Pages\CreateSponsor::route('/create'),
+            'edit' => Pages\EditSponsor::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ database/migrations/2024_03_25_103350_create_sponsors_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sponsors', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('name');
+            $table->string('logo');
+            $table->string('link', 512);
+
+            $table->string('banner_image');
+            $table->string('banner_link', 512);
+
+            $table->string('category');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sponsors');
+    }
+};

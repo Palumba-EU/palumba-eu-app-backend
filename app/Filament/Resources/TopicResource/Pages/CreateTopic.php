d25a312959836a5abcea255069983848ffdd11a1
Jürgen Fitzinger <juergen@bitperfect.at>
2024-03-25 11:32:38 +0100

    Adds topics

---
+++ app/Filament/Resources/TopicResource/Pages/CreateTopic.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\TopicResource\Pages;
+
+use App\Filament\Resources\TopicResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateTopic extends CreateRecord
+{
+    protected static string $resource = TopicResource::class;
+}

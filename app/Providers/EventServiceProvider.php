e1b45094f419aa8cc41d4d300b9660b6cada3d29
Jürgen Fitzinger <juergen@bitperfect.at>
2024-04-15 15:14:22 +0200

    Reverts unnecessary change

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,7 +6,6 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
-use Illuminate\Support\Facades\Log;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -26,6 +25,7 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        //
     }
 
     /**

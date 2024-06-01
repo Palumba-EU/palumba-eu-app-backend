2a924deec699debf6e11359e4708328055eff828
Michael Kudler <michael.kudler@aon.at>
2024-06-01 19:25:06 +0200

    Add graphs to response widgets

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -5,6 +5,8 @@ namespace App\Filament\Widgets;
 use App\Models\Response;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
 
 class StatsOverview extends BaseWidget
 {
@@ -12,12 +14,31 @@ class StatsOverview extends BaseWidget
     {
         $responsesTotal = Response::query()->count();
         $responses7d = Response::query()->since(now()->subWeek())->count();
+        $responses7dChartData =Trend::model(Response::class)
+            ->between(
+                start: now()->subWeek(),
+                end: now(),
+            )
+            ->perHour()
+            ->count();
+
         $responses24h = Response::query()->since(now()->subDay())->count();
+        $responses24hChartData =Trend::model(Response::class)
+            ->between(
+                start: now()->subDay(),
+                end: now(),
+            )
+            ->perMinute()
+            ->count();
 
         return [
             Stat::make('Responses total', $responsesTotal),
-            Stat::make('Responses in the last 7 days', $responses7d),
-            Stat::make('Responses in the last 24h', $responses24h),
+            Stat::make('Responses in the last 7 days', $responses7d)
+                ->chart($responses7dChartData->map(fn (TrendValue $value) => $value->aggregate)->toArray())
+                ->color('success'),
+            Stat::make('Responses in the last 24h', $responses24h)
+                ->chart($responses24hChartData->map(fn (TrendValue $value) => $value->aggregate)->toArray())
+                ->color('success'),
         ];
     }
 }

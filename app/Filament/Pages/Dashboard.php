910d711d1acff78f5ee11e33e2f734e769665e6f
Jürgen Fitzinger <113433977+bitperfect-fitzi@users.noreply.github.com>
2025-02-07 14:45:07 +0100

    Updates crowd in up/download buttons to be more clear (#84)

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -12,11 +12,22 @@ class Dashboard extends \Filament\Pages\Dashboard
     {
         return [
             Action::make('Download Translations')
+                ->icon('heroicon-o-arrow-down-tray')
+                ->label('Download Translations from CrowdIn')
                 ->requiresConfirmation()
+                ->modalHeading('Have you built the Translations?')
+                ->modalDescription('Before downloading, you need to build the Translations in CrowdIn. Go to Tab Translations > Download as ZIP, select "All languages", hit "Build" and wait for the build to finish before continuing.')
+                ->modalSubmitActionLabel('Yes, I built them. Continue')
+                ->modalIcon('heroicon-o-arrow-down-tray')
                 ->action(fn () => DownloadTranslationsFromCrowdInJob::dispatch())
                 ->authorize('sync crowdin'),
             Action::make('Upload Translations')
+                ->icon('heroicon-o-arrow-up-tray')
+                ->label('Sync Source Texts to CrowdIn')
                 ->requiresConfirmation()
+                ->modalDescription('This will upload all english source texts to CrowdIn for translation')
+                ->modalSubmitActionLabel('Yes, upload')
+                ->modalIcon('heroicon-o-arrow-up-tray')
                 ->action(fn () => UploadTranslationsToCrowdInJob::dispatch())
                 ->authorize('sync crowdin'),
         ];
